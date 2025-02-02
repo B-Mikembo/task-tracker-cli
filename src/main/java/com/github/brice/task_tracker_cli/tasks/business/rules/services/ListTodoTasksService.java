b84b749765f9f1f339172980e724154f96d5c894
B-Mikembo <mikembo.brice.pro@gmail.com>
2025-02-02 17:30:52 +0100

    feat: list todo, in progress and done tasks

--- src/main/java/com/github/brice/task_tracker_cli/tasks/business/rules/services/ListTodoTasksService.java
+++ src/main/java/com/github/brice/task_tracker_cli/tasks/business/rules/services/ListTodoTasksService.java
@@ -2,18 +2,20 @@ package com.github.brice.task_tracker_cli.tasks.business.rules.services;
 
 import com.github.brice.task_tracker_cli.tasks.business.repositories.TaskRepository;
 import com.github.brice.task_tracker_cli.tasks.business.rules.entities.Task;
+import com.github.brice.task_tracker_cli.tasks.business.rules.entities.TaskStatus;
 
 import java.util.List;
-import java.util.function.Predicate;
 
-public class ListAllService {
+public class ListTodoTasksService {
     private final TaskRepository taskRepository;
 
-    public ListAllService(TaskRepository taskRepository) {
+    public ListTodoTasksService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
 
     public List<Task> execute() {
-        return taskRepository.findAll();
+        return taskRepository.findAll().stream()
+                .filter(task -> TaskStatus.TODO == task.status())
+                .toList();
     }
 }
